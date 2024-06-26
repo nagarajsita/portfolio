ad16c0a9c7d13a99f2f8a8544e9c42d48120d876
nagarajsita <nagarajsita@gmail.com>
2024-06-26 16:59:24 +0530

    footer modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { LinkPreview } from "./ui/link-preview";
-import image_resume from "./image/resume_v.webp";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
@@ -11,21 +11,24 @@ const Footer = () => {
           className="w-full h-full opacity-15"
         />
       </div>
-      <div className="flex flex-col justify-center items-center h-[40rem] px-4">
-        <p className="text-center font-extrabold text-[20px] md:text-2xl lg:text-4xl mb-10">
+      <div className="flex flex-col items-center h-[40rem] px-4 md:px-8 lg:px-16">
+        <h1 className="text-center font-extrabold text-[32px] md:text-2xl lg:text-4xl mb-5 md:mb-8">
           Ready to discover emerging talent in tech?
-          <br />
+        </h1>
+        <h4 className="text-[15px] md:text-xl lg:text-2xl ">
           Explore my journey and potential through my{" "}
-          <LinkPreview
-            url="https://drive.google.com/file/d/11K8x-Eu9Vl27DbjYEcd9iF7hTXevagF4/view"
-            imageSrc={image_resume}
-            isStatic
-            className="font-bold"
-          >
-            <span className="text-purple">resume</span>
-          </LinkPreview>{" "}
-          - where passion meets practice!
-        </p>
+          <span className="text-purple">resume</span>
+        </h4>
+        <a
+          target="_blank"
+          href="https://drive.google.com/file/d/11K8x-Eu9Vl27DbjYEcd9iF7hTXevagF4/view"
+        >
+          <MagicButton
+            title="View Resume"
+            icon={<FaLocationArrow />}
+            position="right"
+          ></MagicButton>
+        </a>
       </div>
     </footer>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  images: {
-    domains: [
-      "api.microlink.io", // Microlink Image Preview
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
