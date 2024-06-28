8a34e793c4f66570349f1f065dc90c56a7c2d58f
nagarajsita <nagarajsita@gmail.com>
2024-06-28 21:30:53 +0530

    spotlights-fixed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,14 +8,17 @@ const Hero = () => {
     <div id="home" className="pb-20 pt-36">
       <div>
         <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          className="hidden sm:block -top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
         <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]"
+          className="hidden sm:block top-10 left-full h-[80vh] w-[50vw]"
           fill="purple"
         />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight
+          className="hidden sm:block top-28 left-80 h-[80vh] w-[50vw]"
+          fill="blue"
+        />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
