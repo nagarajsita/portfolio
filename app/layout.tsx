7a6e8c99f739dbee21dfd7c0d40bda4782b4cb6e
nagarajsita <nagarajsita@gmail.com>
2024-07-05 17:25:27 +0530

    added vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
